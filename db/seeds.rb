e2d828620f80f96b2da6496efb53c285cf7fc725
Volodia <volodymyrpysklynets@ukd.edu.ua>
2023-05-01 22:15:55 +0300

    movie_catalog

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+# movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Movie.create(
+  [
+    { name: "Mean Girls" , year: 2004 },
+    { name: "The Truman Show" , year: 1998 },
+    { name: "National Lampoon's Vacation" , year: 1983 },
+    { name: "The Mask" , year: 1994 },
+    { name: "The Jungle Book", year: 2016 },
+    { name: "Mrs. Doubtfire", year: 1993 },
+    { name: "Pulp Fiction", year: 1994 },
+    { name: "Suicide Squad", year: 2016 },
+    { name: "Mad Max: Fury Road", year: 2015 },
+    { name: "La La Land", year: 2016 },
+    { name: "The Blues Brothers", year: 1980 },
+    { name: "The Avengers" , year: 2012 },
+    { name: "TMonty Python's Life of Brian" , year: 1979 },
+    { name: "The Gentlemen", year: 2019 },
+  ]
+)
